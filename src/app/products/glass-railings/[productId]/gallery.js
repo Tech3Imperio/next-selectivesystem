a044af0487eef8fc51a0d315ab843795123bb3df
Tech3 <Tech3@imperiorailing.com>
2025-01-17 17:53:43 +0530

    new

--- src/app/products/glass-railings/[productId]/gallery.js
+++ src/app/products/glass-railings/[productId]/gallery.js
@@ -5,6 +5,7 @@ import { notFound, usePathname } from "next/navigation";
 export default GiArtilleryShell = (params) => {
   console.log(params);
   const pathname = usePathname();
+  console.log(pathname);
   const fetchedId = pathname.slice(-1);
   console.log(fetchedId);
   const product = DoorSealProductsPanel.find(
