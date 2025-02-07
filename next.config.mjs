458994fd6bf0c0af4516a45c9d1cc721922ef8f8
tech2 <tech2@imperiorailing.com>
2025-02-07 10:59:06 +0530

    form cred

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,12 @@
 const nextConfig = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
     // Add this line
   },
 };
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 export default nextConfig;
+// next.config.mjs
