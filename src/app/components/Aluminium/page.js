9190c82e3f65faf557985a94b667d8161fbd6166
Tech3 <Tech3@imperiorailing.com>
2025-02-13 17:43:02 +0530

    added canonical in components routes

--- src/app/components/Aluminium/page.js
+++ src/app/components/Aluminium/page.js
@@ -98,6 +98,30 @@ import windows4 from "../../assets/AluminiumProduct/AluminiumWindows/windows4.we
 import windows6 from "../../assets/AluminiumProduct/AluminiumWindows/windows6.webp";
 import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 
+export const metadata = {
+  title: "Aluminium Components",
+  description:
+    "Looking for the best aluminium windows, modern glass railings, invisible grills, stainless steel windows, bathroom partitions, and office partitions? Selective Systems offers high-quality products for homes and offices in Bangalore, Mumbai, Chennai, Pune, and beyond. Contact us today for premium solutions!",
+  keywords: [
+    "aluminium windows Bangalore",
+    "glass railings in Mumbai",
+    "best invisible grills in Chennai",
+    "bathroom glass partitions",
+    "queue manager systems",
+    "stainless steel windows",
+    "aluminium door solutions",
+    "modern office partitions",
+    "balcony glass railings",
+    "modern aluminium window design",
+    "aluminium window suppliers",
+    "selective systems products",
+  ],
+  // url: "https://www.selectivesystems.in/contact",
+  alternates: {
+    canonical: "/components/Aluiminium",
+  },
+};
+
 const AluminiumProductsPanel = [
   {
     id: "fixed-aluminium-window-01",
--- src/app/components/GlassRailing/page.js
+++ src/app/components/GlassRailing/page.js
@@ -377,6 +377,30 @@ const faqItems = [
   },
 ];
 
+export const metadata = {
+  title: "Selective Systems: Glass Railing",
+  description:
+    "Looking for the best aluminium windows, modern glass railings, invisible grills, stainless steel windows, bathroom partitions, and office partitions? Selective Systems offers high-quality products for homes and offices in Bangalore, Mumbai, Chennai, Pune, and beyond. Contact us today for premium solutions!",
+  keywords: [
+    "aluminium windows Bangalore",
+    "glass railings in Mumbai",
+    "best invisible grills in Chennai",
+    "bathroom glass partitions",
+    "queue manager systems",
+    "stainless steel windows",
+    "aluminium door solutions",
+    "modern office partitions",
+    "balcony glass railings",
+    "modern aluminium window design",
+    "aluminium window suppliers",
+    "selective systems products",
+  ],
+  // url: "https://www.selectivesystems.in/contact",
+  alternates: {
+    canonical: "/components/GlassRailing",
+  },
+};
+
 export default function GlassRailingProduct() {
   const [expandedBlog, setExpandedBlog] = useState(null);
   const [expandedFaq, setExpandedFaq] = useState(null);
