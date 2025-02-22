2133a23a99386b5dfe2a163fbbd95d2b254f1856
Tech3 <Tech3@imperiorailing.com>
2025-02-22 14:53:37 +0530

    testing blogs admin panel

---
+++ src/app/testblog/getBlogsData.js
@@ -0,0 +1,44 @@
+"use server";
+
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI_BLOGS;
+
+console.log(uri);
+
+if (!uri) {
+  console.error("MongoDB URI is not defined");
+}
+
+const testSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  urlId: String,
+  keywords: [String],
+});
+
+const TestBlog =
+  mongoose.models.TestBlog || mongoose.model("TestBlog", testSchema);
+
+async function getBlogsData() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
+
+  const blogsData = (await mongoose.models.TestBlog.find()).map((blog) => ({
+    title: blog.title,
+    description: blog.description,
+    urlId: blog.urlId,
+    keywords: blog.keywords,
+  }));
+  console.log(blogsData);
+
+  return blogsData;
+}
+
+export default getBlogsData;
