5a3cd3b9570d343860e54e4d2e9b8efd0f06978c
tech2 <tech2@imperiorailing.com>
2025-03-19 14:57:20 +0530

    metapixel2

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,7 +53,30 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="_hSMrZdm-g-xYfnhnNqkbAI-BaUsqxIKSUAI0S0oVCU"
         />
-
+        {/* Meta Pixel Code */}
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '622230024122248');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=622230024122248&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         {/* You can add other meta tags here */}
 
         {/* Preconnect to Google Fonts and Load Font */}
