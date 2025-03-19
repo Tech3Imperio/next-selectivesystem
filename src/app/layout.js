6d179d0fb75190bbed0cef6f7bae56a28246f3aa
tech2 <tech2@imperiorailing.com>
2025-03-19 14:09:44 +0530

    metapixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,6 +55,21 @@ export default function RootLayout({ children }) {
         />
 
         {/* You can add other meta tags here */}
+        {/* <!-- Meta Pixel Code --> */}
+        <script>
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '622230024122248');
+          fbq('track', 'PageView');`}
+        </script>
+
+        {/* <!-- End Meta Pixel Code --> */}
         {/* Preconnect to Google Fonts and Load Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -68,6 +83,14 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className=" antialiased overflow-x-hidden w-screen">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=622230024122248&ev=PageView&noscript=1"
+          />
+        </noscript>
         <Navbar />
         {children}
         <Whatsapp />
