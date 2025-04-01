f189ddedb6d377ff9a8e9c2d17332068a0371340
Tech3 <Tech3@imperiorailing.com>
2025-04-01 12:34:31 +0530

    Dynamic Blogs Working

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,7 @@ const nextConfig = {
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
     GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
+    MONGODB_URI_BLOGS: process.env.MONGODB_URI_BLOGS,
     // Add this line
   },
 };
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import Blog from "./Blog";
 import getBlogsData from "./getBlogsData";
